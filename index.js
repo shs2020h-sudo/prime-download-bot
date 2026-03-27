5d5fd48dfd1c3eafd47622761e5a7d781a57d640
shs2020h-sudo <shs2020h@gmail.com>
2026-03-27 21:49:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const TelegramBot = require("node-telegram-bot-api");
 const { exec } = require("child_process");
 const fs = require("fs");
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN);
+
+// 🔥 تنظيف أي جلسة قديمة
+bot.deleteWebHook().then(() => {
+  bot.startPolling();
+});
 
 console.log("BOT STARTED");
 
@@ -11,17 +16,18 @@ bot.on("message", (msg) => {
   const url = msg.text;
 
   if (!url || !url.includes("http")) {
-    return bot.sendMessage(chatId, "ابعت لينك بس 👇");
+    return bot.sendMessage(chatId, "ابعت لينك فيديو بس 👇");
   }
 
   bot.sendMessage(chatId, "⏳ جاري التحميل...");
 
+  const fixedUrl = url.replace("vm.tiktok.com", "www.tiktok.com");
   const file = `video_${Date.now()}.mp4`;
 
-  exec(`yt-dlp -f best -o "${file}" "${url}"`, (err) => {
+  exec(`yt-dlp -f "bv*+ba/best" --no-playlist -o "${file}" "${fixedUrl}"`, (err, stdout, stderr) => {
     if (err) {
-      console.log(err);
-      return bot.sendMessage(chatId, "❌ حصل خطأ");
+      console.log(stderr);
+      return bot.sendMessage(chatId, "❌ فشل التحميل");
     }
 
     bot.sendVideo(chatId, file)
