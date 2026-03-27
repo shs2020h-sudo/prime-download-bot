387e09d49b30c21aec63dbed62ab8b08386cb1fd
shs2020h-sudo <shs2020h@gmail.com>
2026-03-27 21:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "البوت شغال يا معلم 🔥");
+});
